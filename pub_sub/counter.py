4224bc13cc82ead7146569cb88762a48df1ce311
Manuel Roglan <manueljoseph113@gmail.com>
2022-12-26 20:31:17 -0500

    initial commit

---
+++ pub_sub/counter.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+
+class CounterNode(Node):
+
+    def __init__(self):
+        super().__init__('counter')
+
+        self.counter = 0
+
+        self.create_timer(
+            1.0, 
+            self.execute     
+        )
+    
+    def execute(self):
+        self.counter += 1
+        self.get_logger().info(str(self.counter))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = CounterNode()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
